f9b7f92ee4655acc54b3dd36c37793f084b20d13
Larabsg <laragomes2009@hotmail.com>
2022-04-05 02:58:24 -0300

    metodo newton aprimorado: cahar X (completo)

--- calculo-numerico/metodo_newton_apri.c
+++ calculo-numerico/metodo_newton_apri.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double funcao(int grau, float ini, float x, float *coefs) {
-    float b;
+float acha_x(int grau, float ini, float x, float *coefs) {
+    static float b;
     
     if(grau > 0) {
         
         b = coefs[grau-1] + ini * x;
-        printf("%d - %f\n",grau, b);        
-        funcao(grau-1, b, x, coefs);
+        printf("%d -> %f\n",grau, b);        
+        acha_x(grau-1, b, x, coefs);
         
     } else {
         return b;
@@ -20,7 +20,7 @@ int main()
     int grau, i;
     float x0, precisao;
     float *coef;
-    //float *aux;
+    float *aux;
     float resultado;
     
     
@@ -28,7 +28,7 @@ int main()
     scanf("%d", &grau);
     
     coef = (float*)malloc(grau * sizeof(float));
-    //aux = (float*)malloc(grau * sizeof(float));
+    aux = (float*)malloc(grau * sizeof(float));
     
     printf("Digite o X0: ");
     scanf("%f", &x0);
@@ -41,8 +41,7 @@ int main()
         scanf("%f", &coef[i]);
     }
     
-    
-    printf("Resultado: %f", funcao(grau, coef[grau], x0, coef));
+    printf("Resultado: %f", acha_x(grau, coef[grau], x0, coef));
     
 
     return 0;
