04340eeee7a278409599b5cd98eb1d178273c2cf
Larabsg <laragomes2009@hotmail.com>
2022-04-05 16:06:37 -0300

    Metodo de newton: achar derivada - ok

--- calculo-numerico/metodo_newton_apri.c
+++ calculo-numerico/metodo_newton_apri.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float acha_x(int grau, float ini, float x, float *coefs) {
-    static float b;
+//static 
+
+float* acha_x(int grau, float ini, float ini_der, float x, float *coefs) {
+    
+    float *b; 
+    float *c; // valor de x na derivada
+
+    b = (float*)malloc(grau * sizeof(float));
+    c = (float*)malloc(grau * sizeof(float));
+
+    //size_t n = sizeof(c)/sizeof(c[grau]);
     
     if(grau > 0) {
         
-        b = coefs[grau-1] + ini * x;
-        printf("%d -> %f\n",grau, b);        
-        acha_x(grau-1, b, x, coefs);
+        b[grau] = coefs[grau-1] + ini * x;
+        c[grau] = b[grau] + ini_der * x;
+
+        printf("b%d -> %f\n", grau-1, b[grau]);        
+        printf("c%d -> %f\n", grau-1, c[grau]);   
+
+        acha_x(grau-1, b[grau], c[grau], x, coefs);
         
     } else {
         return b;
@@ -17,21 +30,21 @@ float acha_x(int grau, float ini, float x, float *coefs) {
 
 int main()
 {
-    int grau, i;
-    float x0, precisao;
+    int grau=3, i;
+    float x0=0.5, precisao;
     float *coef;
     float *aux;
     float resultado;
     
     
-    printf("Digite o grau do polinomio: ");
-    scanf("%d", &grau);
+    //printf("Digite o grau do polinomio: ");
+    //scanf("%d", &grau);
     
     coef = (float*)malloc(grau * sizeof(float));
-    aux = (float*)malloc(grau * sizeof(float));
+    //aux = (float*)malloc(grau * sizeof(float));
     
-    printf("Digite o X0: ");
-    scanf("%f", &x0);
+    //printf("Digite o X0: ");
+    //scanf("%f", &x0);
     
     // printf("Digite o valor da precisão: ");
     // scanf("%f", &precisao);
@@ -40,8 +53,10 @@ int main()
         printf("Digite o valor do coeficiente a%d: ", i);
         scanf("%f", &coef[i]);
     }
-    
-    printf("Resultado: %f", acha_x(grau, coef[grau], x0, coef));
+
+    aux = acha_x(grau, coef[grau], coef[grau], x0, coef);
+
+    printf("Resultado: %f", aux[0]);
     
 
     return 0;
